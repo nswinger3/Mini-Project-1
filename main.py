e9bf185872ee43dba51d99f842b29acb81526ad4
Nathaniel Swinger <146243884+nswinger3@users.noreply.github.com>
2023-10-27 18:19:10 -0400

    added python codebase

--- main.py
+++ main.py
@@ -4,62 +4,82 @@ from util import *
 Model: V. Vajre, M. Naylor, U. Kamath and A. Shehu, "PsychBERT: A Mental Health Language Model for Social Media Mental Health Behavioral Analysis," 2021 IEEE International Conference on Bioinformatics and Biomedicine (BIBM), Houston, TX, USA, 2021, pp. 1077-1082, doi: 10.1109/BIBM52615.2021.9669469.'''
 
 if __name__ == '__main__':
-    global_file_path = '/Users/nswinger/CodingProjects/MiniProject1' # directory location
+    global_file_path = '/data' # directory location
     subreddit = 'c' # picks which subreddits are loaded from full data set, options: 'c', 'mh', 'nmh', 'all'
+    subsample = True # If false, full datasets are used, else uses subsample_size for subsampling
     subsample_size = 453 # number of posts from each subreddit, note: int for balanced, float for unbalanced
-    max_batch_size=5 # number of posts per batch when tokenizing and modeling
-    max_tokens=512 # max number of tokens per post
-    pca_only = False # won't do any classification when False,
-    num_trials = 100 # number of random trials used by classify for significance testing
-    perform_pca_bert=False # performs PCA on BERT embeddings when True, uses n_pca_components, note: auto set to True if pca_only is True
-    perform_pca_feat=False # performs PCA on feature embeddings when True, uses n_pca_components, note: auto set to True if pca_only is True
+    train_model = True # Trains model if true, else skips training and embeddings are generated and evaluated as such
+    batch_size = 15 # number of posts per batch when training and evaluating model
+    num_trials = 20 # number of random trials used by classify for significance testing
+    perform_pca_bert=True # performs PCA on BERT embeddings when True, uses n_pca_components, note: auto set to True if pca_only is True
+    perform_pca_feat=True # performs PCA on feature embeddings when True, uses n_pca_components, note: auto set to True if pca_only is True
     n_pca_components_bert=None # determines the number of components for PCA dimensionality reduction for BERT, note: if None or if >= original # of dimensions, no reduction
     n_pca_components_feat=None # determines the number of components for PCA dimensionality reduction for features, note: if None or if >= original # of dimensions, no reduction
     n_eig_view=20 # determines number of viewed eigenvalues in printed output, no bearing on reduction: note: if > n_pca_components, defaults to n_pca_components
-    np.random.seed(0) # set random seed for subsampling df
+    rand_feat = False # If true, uses randomly generated features instead of given text features, note: meant for code testing purposes
+    seed = 42 # seed for random seed
 
-    print(f'Beginning data loading...\n')
-    start = time.time()
-    reddit_data = load_data(global_file_path, subsample_size=subsample_size, subreddit=subreddit)
-    stop = time.time()
-    print(f'Data loading complete, run time: {stop-start:.2f} s...\n   data has shape [{reddit_data.shape[0]} x {reddit_data.shape[1]}]\n')
 
-    posts = reddit_data.post.values.tolist()
+    np.random.seed(seed) # set random seed for subsampling df
 
-    print(f'Loading pretrained BERT embeddings...\n')
-    psychBERT_embeddings = load_BERT_embeddings(global_file_path, posts, max_batch_size=max_batch_size, max_tokens=max_tokens, subreddit=subreddit)
+    with open(join(global_file_path, f"terminal_output-{datetime.now().strftime('%m-%d-%y %X')}.out"), 'w') as f:
+        sys.stdout = f
 
-    X_bert = psychBERT_embeddings
+        print(f'Parameters of run:')
+        print(f'    subreddit = {subreddit}')
+        print(f'    subsample = {subsample}')
+        print(f'    subsample_size = {subsample_size}')
+        print(f'    train_model = {train_model}')
+        print(f'    batch_size = {batch_size}')
+        print(f'    num_trials = {num_trials}')
+        print(f'    perform_pca_bert = {perform_pca_bert}')
+        print(f'    perform_pca_feat = {perform_pca_feat}')
+        print(f'    n_pca_componenents_bert = {n_pca_components_bert}')
+        print(f'    n_pca_componenents_feat = {n_pca_components_feat}')
+        print(f'    n_eig_view = {n_eig_view}')
+        print(f'    rand_feat = {rand_feat}')
+        print(f'    seed = {seed}\n_____________________________________________________________\n')
 
-    print(f'Beginning feature processing...\n')
-    start = time.time()
-    features = list(reddit_data.columns)
-    features = [n for n in features if n not in ['subreddit', 'author', 'date', 'post']]
-    X_feat = reddit_data[features].values
-    stop = time.time()
-    print(f'Feature processing complete, run time: {stop-start:.2f} s...\n   features have shape [{len(X_feat)} x {len(X_feat[0])}]\n')
+        print(f'Beginning data loading...\n')
+        start = time.time()
+        reddit_data = load_data(global_file_path, subsample, subsample_size, subreddit)
+        stop = time.time()
+        print(f'Data loading complete, run time: {stop-start:.2f} s...\n   data has shape [{reddit_data.shape[0]} x {reddit_data.shape[1]}]\n')
 
-    y = reddit_data.subreddit.values
+        le = preprocessing.LabelEncoder()
+        reddit_data.loc[:, 'subreddit'] = le.fit_transform(reddit_data['subreddit'])
+        token_data = get_preprocessed_for_tokenization(reddit_data, seed)
 
-    if not pca_only:
+        print(f'Loading pretrained BERT embeddings...\n')
+        X_bert, y_bert = load_BERT_embeddings(global_file_path, token_data, batch_size, subreddit, train_model)
+
+        print(f'Beginning feature processing...\n')
+        start = time.time()
+        X_feat, y_feat = get_features(reddit_data)
+        stop = time.time()
+        print(f'Feature processing complete, run time: {stop-start:.2f} s...\n   features have shape [{len(X_feat)} x {len(X_feat[0])}]\n')
+
+        if rand_feat:
+            X_feat = np.random.rand(len(X_feat), len(X_feat[0])) # testing classification in random case instead of features
+
+
         print(f'Beginning classification...\n')
 
-    start_b = time.time()
-    accs_bert, f1s_bert = classify(global_file_path, X_bert, y, n_trials=num_trials, perform_pca=perform_pca_bert, n_pca_components=n_pca_components_bert, vec_name='BERT', n_eig_view=n_eig_view, pca_only=pca_only)
-    stop_b = time.time()
+        start_b = time.time()
+        accs_bert, f1s_bert = classify(global_file_path, X_bert, y_bert, n_trials=num_trials, perform_pca=perform_pca_bert, n_pca_components=n_pca_components_bert, vec_name='BERT', n_eig_view=n_eig_view)
+        stop_b = time.time()
 
-    start_f = time.time()
-    accs_feat, f1s_feat = classify(global_file_path, X_feat, y, n_trials=num_trials, perform_pca=perform_pca_feat, n_pca_components=n_pca_components_feat, vec_name='Features', n_eig_view=n_eig_view, pca_only=pca_only)
-    stop_f = time.time()
+        start_f = time.time()
+        accs_feat, f1s_feat = classify(global_file_path, X_feat, y_feat, n_trials=num_trials, perform_pca=perform_pca_feat, n_pca_components=n_pca_components_feat, vec_name='Features', n_eig_view=n_eig_view)
+        stop_f = time.time()
 
-    if not pca_only:
         acc_bert = get_stats(accs_bert)
         f1_bert = get_stats(f1s_bert)
         acc_feat = get_stats(accs_feat)
         f1_feat = get_stats(f1s_feat)
 
         print(f'Classification results over {num_trials} trials:')
-        print(f"    BERT:    Runtime: {stop_b-start_b:.2f} s    ---    mean accuracy: {acc_bert['mu']:.2f}, σ: {acc_bert['sigma']:.2f}    ---    mean f1: {f1_bert['mu']:.2f}, σ: {f1_bert['sigma']:.2f}")
-        print(f"    Features:    Runtime: {stop_f-start_f:.2f} s    ---    mean accuracy: {acc_feat['mu']:.2f}, σ: {acc_feat['sigma']:.2f}    ---    mean f1: {f1_feat['mu']:.2f}, σ: {f1_feat['sigma']:.2f}")
+        print(f"    BERT:        Runtime: {stop_b-start_b:.2f} s    ---    mean accuracy: {acc_bert['mu']:.2f}, σ: {acc_bert['sigma']:.2f}    ---    mean f1: {f1_bert['mu']:.2f}, σ: {f1_bert['sigma']:.2f}")
+        print(f"    Features:    Runtime: {stop_f-start_f:.2f} s    ---    mean accuracy: {acc_feat['mu']:.2f}, σ: {acc_feat['sigma']:.2f}    ---    mean f1: {f1_feat['mu']:.2f}, σ: {f1_feat['sigma']:.2f}\n")
 
-        plot_classification_results(global_file_path, accs_bert, f1s_bert, accs_feat, f1s_feat)
+        print(f'\n    Results saved at {plot_classification_results(global_file_path, accs_bert, f1s_bert, accs_feat, f1s_feat)}\n')
--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForMaskedLM, AutoTokenizer
+from transformers import AutoModelForMaskedLM, AutoModelForSequenceClassification, AutoTokenizer, get_scheduler
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
@@ -10,10 +10,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import f1_score
+from sklearn import preprocessing
 import matplotlib.pyplot as plt
 from datetime import datetime
 import time
 from collections import OrderedDict
+import sys
+import evaluate
+from datasets import Dataset, DatasetDict, concatenate_datasets
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from tqdm.auto import tqdm
 
 
 def get_subreddits(v):
@@ -71,27 +78,7 @@ def get_subreddits(v):
         return full_reddit
 
 
-def load_model(global_file_path):
-    try:
-        pytorch_lm = torch.load(join(global_file_path, 'model.pth'))
-    except FileNotFoundError:
-        pytorch_lm = AutoModelForMaskedLM.from_pretrained('mnaylor/psychbert-cased', from_flax=True,
-                                                          output_hidden_states=True)
-        torch.save(pytorch_lm, join(global_file_path, 'model.pth'))
-    pytorch_tk = AutoTokenizer.from_pretrained("mnaylor/psychbert-cased")
-    return pytorch_lm, pytorch_tk
-
-
-def get_max_subsample_size(path, fs):
-    min_len = 1000000
-    for f in fs:
-        df = clean(pd.read_csv(join(path, f), engine='python', on_bad_lines='skip'))
-        if len(df) < min_len:
-            min_len = len(df)
-    return min_len
-
-
-def load_data(global_file_path, file_name=None, subsample=True, subsample_size=10, subreddit='c', clean_data=True):
+def load_data(global_file_path, subsample, subsample_size, subreddit, clean_data=True, file_name=None):
     # subsamples balanced across subreddits with subsample_size type int, unbalanced with float
     path = join(global_file_path, 'reddit_mental_health_dataset_nswinger')
     sub_names = {'c': 'condition', 'mh': 'mental_health', 'nmh': 'non_mental_health', 'all': 'full_reddit'}
@@ -114,6 +101,8 @@ def load_data(global_file_path, file_name=None, subsample=True, subsample_size=1
                     subsample_size = max_subsample_size
                     save_file = join(global_file_path, f'{sub_names[subreddit]}_df_{subsample_size}.pkl')
 
+            progress_bar = tqdm(range(len(all_files)))
+            progress_bar.set_description('Loading Data')
             all_files.sort()
             df = pd.DataFrame()
             for f in all_files:
@@ -123,6 +112,10 @@ def load_data(global_file_path, file_name=None, subsample=True, subsample_size=1
                 if subsample:
                     df2 = subsample_df(df2, subsample_size)
                 df = pd.concat([df, df2])
+                progress_bar.update(1)
+
+            progress_bar.close()
+            print()
             if 'covid19_total' in df.columns:
                 df = df.drop(columns=['covid19_total'])
             df.to_pickle(save_file)
@@ -138,7 +131,6 @@ def load_data(global_file_path, file_name=None, subsample=True, subsample_size=1
         df = subsample_df(df, subsample_size)
 
     return df
-
 
 
 def subsample_df(df, subsample):
@@ -161,49 +153,36 @@ def clean(df):
     return reddit_data
 
 
-def tokenize(global_file_path, tokenizer, model, posts, max_batch_size, max_tokens, subreddit):
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
-    num_batches = int(np.ceil(len(posts) / max_batch_size))
-    post_batches = np.array_split(posts, num_batches)
-
-    model.eval()
-    model.to(device)
-
-    embeddings = []
-
-    print(f'Tokenizing posts and feeding to pretrained BERT model...\n')
-    start = time.time()
-    for i, post_batch in enumerate(post_batches):
-        encoded_input = tokenizer(post_batch.tolist(), padding=True, truncation=True, max_length=max_tokens, return_tensors="pt")
-
-        # Move tokenized data to GPU just before forwarding through the model
-        encoded_input = {k: v.to(device) for k, v in encoded_input.items()}
-
-        with torch.no_grad():
-            output = model(**encoded_input)
-            hidden_states = output.hidden_states
-
-            token_vecs = hidden_states[-2]
-            sentence_embedding = torch.mean(token_vecs, dim=1)
-            embeddings.extend(sentence_embedding.cpu().numpy())
-
-    stop = time.time()
-    print(f'BERT embeddings generated, run time: {stop-start:.2f} s...\n   embedding set has shape [{len(embeddings)} x {len(embeddings[0])}]\n')
-    np.save(join(global_file_path, f'BERT_embeddings-{subreddit}-mb{max_batch_size}-mt{max_tokens}-{len(embeddings)}x{len(embeddings[0])}'), embeddings)
-
-    return embeddings
+def get_max_subsample_size(path, fs):
+    min_len = 1000000
+    for f in fs:
+        df = clean(pd.read_csv(join(path, f), engine='python', on_bad_lines='skip'))
+        if len(df) < min_len:
+            min_len = len(df)
+    return min_len
 
 
-def get_embedding(sentence, posts, embeddings):
-    index = posts.index(sentence)
-    return embeddings[index]
+def get_preprocessed_for_tokenization(df, seed):
+    df_subset = df[['subreddit', 'post']]
+    train_df, test_df = train_test_split(df_subset, test_size=0.2, random_state=seed)
+    train_dataset = Dataset.from_pandas(train_df).rename_column('subreddit', 'labels').rename_column('post', 'text').remove_columns(["__index_level_0__"])
+    test_dataset = Dataset.from_pandas(test_df).rename_column('subreddit', 'labels').rename_column('post', 'text').remove_columns(["__index_level_0__"])
+    dataset = DatasetDict({"train": train_dataset, "test": test_dataset})
+
+    return dataset
 
 
-def load_BERT_embeddings(global_file_path, posts, max_batch_size=5, max_tokens=512, subreddit='c'):
+def get_features(df):
+    features = list(df.columns)
+    features = [n for n in features if n not in ['subreddit', 'author', 'date', 'post']]
+    return df[features].values, df['subreddit'].values.tolist()
+
+
+def load_BERT_embeddings(global_file_path, dataset, batch_size, subreddit, train_model):
     try:
         start = time.time()
-        embeddings = np.load(join(global_file_path, f'BERT_embeddings-{subreddit}-mb{max_batch_size}-mt{max_tokens}-{len(posts)}x768.npy'))
+        total_posts = sum(len(dataset[split]['text']) for split in dataset)
+        embeddings = np.load(join(global_file_path, f'BERT_embeddings-{subreddit}-batch{batch_size}-train{train_model}-emb{total_posts}x768.npy'))
         stop = time.time()
         print(f'Pretrained BERT embeddings loaded, run time: {stop-start:.2f} s...\n   embedding set has shape [{len(embeddings)} x {len(embeddings[0])}]\n')
     except FileNotFoundError:
@@ -211,25 +190,189 @@ def load_BERT_embeddings(global_file_path, posts, max_batch_size=5, max_tokens=5
 
         print(f'Loading BERT model and tokenizer...\n')
         start = time.time()
-        psychBERT_model, tokenizer = load_model(global_file_path)
+        model, tokenizer = load_model(global_file_path, subreddit)
         stop = time.time()
         print(f'BERT model and tokenizer loaded, run time: {stop-start:.2f} s...\n')
 
-        embeddings = tokenize(global_file_path, tokenizer, psychBERT_model, posts, max_batch_size, max_tokens, subreddit)
+        embeddings = generate_embeddings(global_file_path, tokenizer, model, dataset, batch_size, subreddit, train_model)
+
+    return embeddings, dataset['train']['labels'] + dataset['test']['labels']
+
+
+def load_model(global_file_path, subreddit):
+    try:
+        pytorch_lm = torch.load(join(global_file_path, f'model_{subreddit}.pth'))
+    except FileNotFoundError:
+        pytorch_lm = AutoModelForSequenceClassification.from_pretrained('mnaylor/psychbert-cased', from_flax=True, output_hidden_states=True, num_labels=len(get_subreddits(subreddit)))
+        torch.save(pytorch_lm, join(global_file_path, f'model_{subreddit}.pth'))
+    pytorch_tk = AutoTokenizer.from_pretrained("mnaylor/psychbert-cased")
+    return pytorch_lm, pytorch_tk
+
+
+def tokenize(tokenizer, dataset, batch_size):
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True, )
+
+    print(f'Tokenizing posts...\n')
+    start = time.time()
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+    tokenized_datasets.set_format("torch")
+
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+    small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+    combined_dataset = concatenate_datasets([tokenized_datasets["train"], tokenized_datasets["test"]])
+
+    train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=batch_size)
+    eval_dataloader = DataLoader(small_eval_dataset, batch_size=batch_size)
+    full_dataloader = DataLoader(combined_dataset, batch_size=batch_size)
+    stop = time.time()
+    print(f'\nTokenization complete, run time: {stop-start:.2f} s...\n')
+
+    return full_dataloader, train_dataloader, eval_dataloader
+
+
+def train_m(model, train_dataloader):
+    print(f'Training model...\n')
+    start = time.time()
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+
+    num_epochs = 3
+    num_training_steps = num_epochs * len(train_dataloader)
+    lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+
+    progress_bar = tqdm(range(num_training_steps))
+    progress_bar.set_description('Training Model')
+
+    model.to(device)
+
+    model.train()
+    for epoch in range(num_epochs):
+        for batch in train_dataloader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+
+            loss = outputs.loss
+            loss.backward()
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+
+    progress_bar.close()
+    stop = time.time()
+    print(f'\nModel training complete, run time: {stop-start:.2f} s...\n')
+    return model
+
+
+def evaluate_m(model, eval_dataloader):
+    print(f'\nEvaluating model...\n')
+    start = time.time()
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    acc_metric = evaluate.load("accuracy")
+    f1_metric = evaluate.load("f1")
+
+    model.eval()
+
+    progress_bar = tqdm(range(len(eval_dataloader)))
+    progress_bar.set_description('Evaluating Model')
+    for batch in eval_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        acc_metric.add_batch(predictions=predictions, references=batch["labels"])
+        f1_metric.add_batch(predictions=predictions, references=batch["labels"])
+        progress_bar.update(1)
+
+    progress_bar.close()
+    stop = time.time()
+    print(f'\nModel evaluation complete, run time: {stop-start:.2f} s...')
+    print(f'   evalution accuracy metric: {acc_metric.compute()}\n   evaluation f1 metric: {f1_metric.compute(average="macro")}\n')
+    return model
+
+
+def generate_embeddings(global_file_path, tokenizer, model, dataset, batch_size, subreddit, train_model):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    full_dataloader, train_dataloader, eval_dataloader = tokenize(tokenizer, dataset, batch_size)
+    if train_model:
+        model = train_m(model, train_dataloader)
+        model = evaluate_m(model, eval_dataloader)
+    else:
+        model.to(device)
+        model.eval()
+
+    print(f'\nGenerating embeddings...\n')
+    start = time.time()
+
+    progress_bar = tqdm(range(len(full_dataloader)))
+    progress_bar.set_description('Generating Embeddings')
+    embeddings = []
+    for batch in full_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+            hidden_states = outputs.hidden_states
+            token_vecs = hidden_states[-2]
+            sentence_embedding = torch.mean(token_vecs, dim=1)
+            embeddings.extend(sentence_embedding.cpu().numpy())
+
+        progress_bar.update(1)
+
+    progress_bar.close()
+    stop = time.time()
+    print(f'BERT embeddings generated, run time: {stop-start:.2f} s...\n   embedding set has shape [{len(embeddings)} x {len(embeddings[0])}]\n')
+    np.save(join(global_file_path, f'BERT_embeddings-{subreddit}-batch{batch_size}-train{train_model}-emb{len(embeddings)}x{len(embeddings[0])}'), embeddings)
 
     return embeddings
 
 
-def classify(global_file_path, X, y, n_trials=50, perform_pca=False, n_pca_components=150, n_eig_view=20, vec_name='', pca_only=False):
-    if pca_only:
-        perform_pca = True
+def do_pca(X_train, X_test, n_pca_components, evrs, evs):
+    if n_pca_components is not None and n_pca_components >= len(X_train[0]):
+        n_pca_components = None
 
+    pca = PCA(n_components=n_pca_components)
+
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.transform(X_test)
+
+    explained_variance_ratio = pca.explained_variance_ratio_
+    explained_variance = pca.explained_variance_
+    evrs.append(explained_variance_ratio)
+    evs.append(explained_variance)
+
+    return X_train, X_test, evrs, evs
+
+
+def eval_pca(global_file_path, evrs, evs, n_eig_view, vec_name, n_trials):
+    evr_stats = get_stats(evrs, batch=True)
+    ev_stats = get_stats(evs, batch=True)
+
+    if n_eig_view > evr_stats['mu'].size:
+        n_eig_view = evr_stats['mu'].size
+    print(f'\nPCA on {vec_name} complete...')
+    print(f"   proportion of explained variance by {n_eig_view} components: {evr_stats['mu'][:n_eig_view].sum():.2f}")
+    print(f"   eigenvalues of first {n_eig_view} components: {np.round(ev_stats['mu'][:n_eig_view],2)}\n")
+
+    print(f'\n results saved at {plot_PCA(global_file_path, evr_stats, ev_stats, vec_name, n_trials)}\n')
+
+
+def classify(global_file_path, X, y, n_trials, perform_pca, n_pca_components, n_eig_view, vec_name):
     f1s = []
     accs = []
 
     if perform_pca:
         print(f'Performing PCA on {vec_name} embeddings...')
         evrs, evs = [], []
+
+    progress_bar = tqdm(range(n_trials))
+    progress_bar.set_description(f'Classifying {vec_name}')
     for n in range(n_trials):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -238,37 +381,21 @@ def classify(global_file_path, X, y, n_trials=50, perform_pca=False, n_pca_compo
         X_test = sc.transform(X_test)
 
         if perform_pca:
-            if n_pca_components is not None and n_pca_components >= len(X_train[0]):
-                n_pca_components = None
+            X_train, X_test, evrs, evs = do_pca(X_train, X_test, n_pca_components, evrs, evs)
 
-            pca = PCA(n_components=n_pca_components)
+        clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        accuracy = clf.score(X_test, y_test)
+        f1 = f1_score(y_test, y_pred, average='macro')
+        f1s.append(f1)
+        accs.append(accuracy)
 
-            X_train = pca.fit_transform(X_train)
-            X_test = pca.transform(X_test)
+        progress_bar.update(1)
 
-            explained_variance_ratio = pca.explained_variance_ratio_
-            explained_variance = pca.explained_variance_
-            evrs.append(explained_variance_ratio)
-            evs.append(explained_variance)
-
-        if not pca_only:
-            clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            accuracy = clf.score(X_test, y_test)
-            f1 = f1_score(y_test, y_pred, average='macro')
-            f1s.append(f1)
-            accs.append(accuracy)
+    progress_bar.close()
 
     if perform_pca:
-        evr_stats = get_stats(evrs, batch=True)
-        ev_stats = get_stats(evs, batch=True)
-
-        if n_eig_view > evr_stats['mu'].size:
-            n_eig_view = evr_stats['mu'].size
-        print(f"   proportion of explained variance by {n_eig_view} components: {evr_stats['mu'][:n_eig_view].sum():.2f}")
-        print(f"   eigenvalues of first {n_eig_view} components: {np.round(ev_stats['mu'][:n_eig_view],2)}\n")
-
-        plot_PCA(global_file_path, evr_stats, ev_stats, vec_name, n_trials)
+        eval_pca(global_file_path, evrs, evs, n_eig_view, vec_name, n_trials)
 
     return accs, f1s
 
@@ -314,10 +441,10 @@ def plot_classification_results(global_file_path, BERT_accs, BERT_f1s, FEAT_accs
     fig, ax = plt.subplots()
 
     # plot all trials
-    ax.plot(x, BERT_accs, marker=',', color='#ecf469', linestyle='', label='BERT accuracies')
-    ax.plot(x * 2, FEAT_accs, marker=',', color='#b2dd53', linestyle='', label='Feature accuracies')
-    ax.plot(x * 3, BERT_f1s, marker=',', color='#53c14f', linestyle='', label='BERT f1s')
-    ax.plot(x * 4, FEAT_f1s, marker=',', color='#31945a', linestyle='', label='Feature f1s')
+    ax.plot(x, BERT_accs, marker='.', ms=4, color='#2a623d', linestyle='', label='BERT accuracies')
+    ax.plot(x * 2, FEAT_accs, marker='.', ms=4, color='#222f5b', linestyle='', label='Feature accuracies')
+    ax.plot(x * 3, BERT_f1s, marker='.', ms=4, color='#1a472a', linestyle='', label='BERT f1s')
+    ax.plot(x * 4, FEAT_f1s, marker='.', ms=4, color='#0e1a40', linestyle='', label='Feature f1s')
 
     # plot means and error bars
     for i, stat in enumerate(stats.values()):
@@ -334,10 +461,10 @@ def plot_classification_results(global_file_path, BERT_accs, BERT_f1s, FEAT_accs
 
     ax.set_xticks((1.15, 2.15, 3.15, 4.15))
     ax.set_xticklabels(stats.keys())
-    ax.set_xlabel('Accuracy Metric                                F1 Metric')
+    ax.set_xlabel('Accuracy Metric                                                                    F1 Metric      ')
 
     ax.set_yticks(np.linspace(0,1,11))
-    ax.set_yticklabels(np.linspace(0,1,11))
+    ax.set_yticklabels((0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0))
     ax.set_ylabel('Scores')
     ax.set_title('Classification Results')
     ax.legend()
@@ -346,6 +473,8 @@ def plot_classification_results(global_file_path, BERT_accs, BERT_f1s, FEAT_accs
     plt.tight_layout()
     plt.savefig(f'{results_file}')
     plt.show()
+
+    return results_file
 
 
 def plot_PCA(global_file_path, evr_stats, ev_stats, vec_name, num_trials):
@@ -398,6 +527,8 @@ def plot_PCA(global_file_path, evr_stats, ev_stats, vec_name, num_trials):
     plt.tight_layout()
     plt.savefig(f'{results_file}')
     plt.show()
+
+    return results_file
 
 
 def tens_ceil(x):
